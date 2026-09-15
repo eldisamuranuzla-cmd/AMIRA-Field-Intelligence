4f87962b02f2f98cf951d2a7d5ce6119eaff349f
eldisamuranuzla-cmd <eldisamuranuzla@gmail.com>
2026-09-15 12:12:51 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-const CACHE="amira-v18-worker-item-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./astra-agro-50-header.png","./astra-agro-logo-compact.png","./astra-agro-logo.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener("fetch",e=>{const req=e.request;if(req.method!=="GET")return;e.respondWith(fetch(req).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(req,c)).catch(()=>{});return r}).catch(()=>caches.match(req).then(r=>r||caches.match("./index.html"))))});
+const CACHE='amira-v21-history-by-date';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./astra-agro-50-header.png','./astra-agro-logo-compact.png','./astra-agro-logo.png']))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
