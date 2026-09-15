f9ccfafcd7df85afff8fc3d7768e33b8200c64ee
eldisamuranuzla-cmd <eldisamuranuzla@gmail.com>
2026-09-15 07:50:17 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='amira-v16-pwa-v1';
+const SHELL=['./','./index.html','./manifest.webmanifest','./astra-agro-50-header.png','./astra-agro-logo-compact.png','./astra-agro-logo.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
